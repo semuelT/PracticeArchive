ad98aa6165640725a4e905b9762c7ada491b5ea2
semuelT <semueljuliotangkuman@gmail.com>
2025-07-23 10:14:49 +0000

    Solved "Counting Clauses"; Wasn't hard, just read properly;

---
+++ Difficulty 1.6/countingclauses.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int clausescount;
+    cin >> clausescount;
+    if (clausescount >= 8) {
+        cout << "satisfactory";
+    } else {
+        cout << "unsatisfactory";
+    }
+}
